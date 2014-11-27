a7c559b5c17c283d1240c1f27822551e8f226abd
JK <emptist@googlegroups.com>
2014-11-27 20:50:34 +0800

    modified

--- usersnet/usersnet.js
+++ usersnet/usersnet.js
@@ -1,20 +1,18 @@
+AdminConfig = { 
+adminEmails: ['j@k.com'], 
+collections: 
+{ 
+  Guests: {}, 
+} 
+};
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+  Meteor.startup(function () {
+    Guests.add();
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-}
+};
+  
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
